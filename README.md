c56664566cdb85b5c5bd4c4bfae7ddf55c76dff9
Nikhil Nair <niknair31898@gmail.com>
2024-02-13 16:12:26 -0600

    Added funcitonality

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
-# EYES-Win
+# EYES: Environment for Your Screenshots
 
-`pyinstaller --onefile --windowed main.py`
+This Python application, called EYES (Environment for Your Screenshots), is designed to capture screenshots at regular intervals, process them, and interact with various AI models for analysis. The application provides a user interface for configuration and control.
+
+## Installation
+
+1. Clone the repository
+
+2. Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the `main.py` script to start the application:
+
+```bash
+python main.py
+```
+
+or create executable with:
+
+```bash
+pyinstaller --onefile --windowed main.py
+```
+
+Once the application starts, you'll see a graphical user interface (GUI) where you can configure the interval between screenshots, select the desired quality, choose the AI model for analysis, and start or stop the screenshot process.
+
+## Features
+
+- **Automatic Screenshot Capture**: EYES captures screenshots at regular intervals from all connected monitors.
+
+- **Quality Adjustment**: The application allows you to adjust the quality of the captured screenshots to optimize file size and clarity.
+
+- **AI Model Integration**: EYES integrates with two AI models for image analysis: GPT (OpenAI) and Moondream. These models can analyze the captured screenshots and provide insights based on the selected model.
+
+- **Database Storage**: Captured screenshots and corresponding AI analysis results are stored in a SQLite database (`data/screenshots.db`) for future reference.
+
+## Configuration
+
+- **Interval**: Set the time interval (in seconds) between consecutive screenshots.
+
+- **System Prompt**: Customize the default prompt used when interacting with the AI models.
+
+- **Quality**: Choose the desired quality percentage for the captured screenshots (25%, 50%, or 75%).
+
+- **Model Selection**: Select the AI model (GPT or Moondream) for image analysis.
+
+## Dependencies
+
+- Python 3.x
+- `base64`
+- `sqlite3`
+- `tempfile`
+- `requests`
+- `pyautogui`
+- `tkinter`
+- `PIL`
+- `gradio_client`
+- `dotenv`
+- `screeninfo`
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions, enhancements, or bug fixes, feel free to open an issue or create a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+**EYES** - Environment for Your Screenshots - Developed by [Your Name](https://github.com/your_username)
--- main.py
+++ main.py
@@ -1,45 +1,45 @@
+# region Packages
 import os
+import io
+import time
 import base64
+import sqlite3
+import tempfile
+import requests
+import pyautogui
 import tkinter as tk
+from PIL import Image
 from tkinter import ttk
 from threading import Thread
-import time
-import sqlite3
-import requests
-from PIL import Image
 from dotenv import load_dotenv
-from screeninfo import get_monitors
 from gradio_client import Client
-from transformers import AutoModelForCausalLM, CodeGenTokenizerFast as Tokenizer
-import io
-import pyautogui
+from screeninfo import get_monitors
+# endregion
 
-# Load environment variables from .env file
+# region Setup
+# Env Vars
 load_dotenv()
-
-# OpenAI API Key
 api_key = os.getenv('OPENAI_API_KEY')
 
-# Global variables to manage the screenshot loop
+# Global variables
 running = False
-interval = 5000
+interval = 2
 db_path = 'data/screenshots.db'
 model_selection = None
 quality_selection = None
 default_system_prompt = "What's in this image?"
+# endregion
 
-def encode_image(image_bytes):
-    """Encode image bytes to base64."""
-    return base64.b64encode(image_bytes).decode('utf-8')
-
+# region Image LLM Related
 def send_image_to_api(image_bytes):
     """Determine which API to call based on the model selection and send the screenshot."""
+
     print(f'Sending Screenshot to API...')
     
-    if model_selection.get() == "GPT-4V":
+    if model_selection.get() == "GPT":
         response = call_open_ai_api(image_bytes)
     elif model_selection.get() == "Moondream":
-        response = call_moondream_local(image_bytes)
+        response = call_moondream_api(image_bytes)
     else:
         print("Invalid model selection.")
         return None
@@ -89,9 +89,13 @@ def call_open_ai_api(image_bytes):
 def call_moondream_api(image_bytes):
     print(f'Calling Moondream API...')
 
-    # Convert bytes image to a base64 string for URL
-    base64_image = encode_image(image_bytes)
-    # image_data_url = f"data:image/jpeg;base64,{base64_image}"
+    # Load the image from bytes for a sanity check or manipulation if needed
+    img = Image.open(io.BytesIO(image_bytes))
+
+    # Save the image temporarily
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.jpeg') as tmpfile:
+        img.save(tmpfile, format="JPEG")
+        tmpfile_path = tmpfile.name
 
     # Initialize the Gradio client with your Moondream API URL
     client = Client("https://vikhyatk-moondream1.hf.space/--replicas/1hkz3/")
@@ -99,41 +103,25 @@ def call_moondream_api(image_bytes):
     # Make the prediction (API call)
     try:
         result = client.predict(
-            "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",   # This should be adjusted if your API expects a file path or a different format
-            default_system_prompt,    # Adjust this as per the requirements of your API
-            api_name="/answer_question"  # Update this endpoint as per your actual Moondream API endpoint
+            tmpfile_path,
+            default_system_prompt,  
+            api_name="/answer_question"
         )
         print("Moondream API Response:", result)
         return result
     except Exception as e:
         print("An error occurred while calling Moondream API:", str(e))
         return ""
-def call_moondream_local(image_bytes):
-    print(f'Calling Moondream Local...')
+    finally:
+        # Optionally delete the temp file if not needed anymore
+        os.unlink(tmpfile_path)
+def encode_image(image_bytes):
+    """Encode image bytes to base64."""
+    return base64.b64encode(image_bytes).decode('utf-8')
+# endregion
 
-    model_id = "vikhyatk/moondream1"
-    model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
-    tokenizer = Tokenizer.from_pretrained(model_id)
-    # enc_image = model.encode_image(image_bytes)
-
-    # Make the prediction
-    try:
-        result = model.answer_question(image_bytes, "<QUESTION>", tokenizer)
-
-        print("Moondream Local Response:", result)
-        return result
-    except Exception as e:
-        print("An error occurred while calling Moondream Local:", str(e))
-        return ""
-
+# region Image Related
 def take_screenshot():
-    print(f'Took Screenshot!')
-
-    screenshot = pyautogui.screenshot()
-    img_byte_arr = io.BytesIO()
-    screenshot.save(img_byte_arr, format='JPEG')
-    return img_byte_arr.getvalue()
-def take_screenshot_better():
     screenshots = []
     for m in get_monitors():
         print(f"Capturing screen: {m.name} at resolution {m.width}x{m.height}")
@@ -166,17 +154,7 @@ def take_screenshot_better():
         img_byte_arr = io.BytesIO()
         screenshots[0].save(img_byte_arr, format='JPEG')
         return img_byte_arr.getvalue()
-
-def downscale_image(image):
-    basewidth = 320
-    img = Image.open(io.BytesIO(image))
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    return img_byte_arr.getvalue()
-def downscale_image_better(image_bytes, quality=90):
+def downscale_image(image_bytes, quality=90):
     """
     Downscale the image to a specified width and adjust quality.
     
@@ -195,7 +173,9 @@ def downscale_image_better(image_bytes, quality=90):
     img.save(img_byte_arr, format='JPEG', quality=quality)
     
     return img_byte_arr.getvalue()
+# endregion
 
+# region DB Related
 def initialize_db(db_file):
     print(f'Initializing DB...')
 
@@ -207,7 +187,6 @@ def initialize_db(db_file):
     conn.close()
 
     print(f'Initialized\n')
-
 def save_to_db(timestamp, image, response):
     print(f'Saving to DB...')
 
@@ -220,13 +199,17 @@ def save_to_db(timestamp, image, response):
     conn.close()
 
     print(f'Saved!\n')
+# endregion
 
+# region Primary Loop Related
 def screenshot_loop():
+    print(f'Started Screenshot Loop...')
+
     global running, interval
 
-    while running:
-        image_bytes = take_screenshot_better()
-        
+    while running:        
+        print(f'Running')
+
         # Convert the quality selection to an appropriate JPEG quality integer.
         quality_factor = str(quality_selection.get())
         if quality_factor == '75%':
@@ -238,12 +221,14 @@ def screenshot_loop():
         else:
             jpeg_quality = 50
 
-        downscaled_image = downscale_image_better(image_bytes, quality=jpeg_quality)
-        response = send_image_to_api(downscaled_image)
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-        # Convert response to a string or extract the needed info from it before saving
+        image_bytes = take_screenshot()
+        downscaled_image = downscale_image(image_bytes, quality=jpeg_quality)
+        response = send_image_to_api(downscaled_image)
         response_text = str(response)
+
         save_to_db(timestamp, downscaled_image, response_text)
+
         time.sleep(interval)
 
 def start_screenshot_process():
@@ -252,12 +237,13 @@ def start_screenshot_process():
     running = True
     thread = Thread(target=screenshot_loop)
     thread.start()
-
 def stop_screenshot_process():
     print(f'Stopped Screenshot Process')
     global running
     running = False
+# endregion
 
+# region UI Related
 def update_interval(new_interval):
     print(f'Updating Interval')
     global interval
@@ -277,15 +263,13 @@ def on_closing():
 def create_ui():
     print(f'Building UI...\n')
 
-    global db_path, root, default_system_prompt, model_selection, quality_selection
+    global root, db_path, default_system_prompt, model_selection, quality_selection
 
     initialize_db(db_path) 
 
     root = tk.Tk()
     root.title("EYES")
-
-    # Set default window size
-    root.geometry('600x400')  # Width x Height
+    root.geometry('600x400')
 
     # Inputs
     tk.Label(root, text="Interval (seconds)").pack()
@@ -300,14 +284,16 @@ def create_ui():
     
     # Dropdowns
     tk.Label(root, text="Select Quality:").pack()
-    quality_selection = ttk.Combobox(root, values=["75%", "50%", "25%"])
+    quality_list = ["25%", "50%", "75%"]
+    quality_selection = ttk.Combobox(root, values=quality_list)
     quality_selection.pack()
-    quality_selection.set("25%")
+    quality_selection.set(quality_list[0])
     
     tk.Label(root, text="Select Model:").pack()
-    model_selection = ttk.Combobox(root, values=["GPT-4V", "Moondream"])
+    model_list = ["GPT", "Moondream"]
+    model_selection = ttk.Combobox(root, values=model_list)
     model_selection.pack()
-    model_selection.set("GPT-4V")
+    model_selection.set(model_list[0])
 
     # Buttons
     tk.Button(root, text="Start", command=lambda: start_screenshot_process()).pack()
@@ -316,9 +302,10 @@ def create_ui():
     tk.Button(root, text="Update Interval", command=lambda: update_interval(interval_entry.get())).pack()
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
+
     root.mainloop()
+# endregion
 
 if __name__ == "__main__":
     print(f'Starting EYES...\n')
-
     create_ui()
